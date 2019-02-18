98a14360de779d525bf27a627a05831a5090c9ef
Steven Frieson <sfrieson@gmail.com>
2019-02-18 13:39:18 -0500

    Adds lambda handler for stationStatus

---
+++ data-harvest/stationStatusHarvester.js
@@ -0,0 +1,55 @@
+const https = require('https');
+
+const AWS = require('aws-sdk');
+const S3 = new AWS.S3();
+
+const BUCKET = 'will-there-be-bikes-data';
+const ENDPOINT = 'https://gbfs.citibikenyc.com/gbfs/en/station_status.json';
+
+function writeObject (filename, data) {
+  return new Promise(function (resolve, reject) {
+    S3.putObject({
+      Key: filename,
+      Body: data,
+      Bucket: BUCKET
+    }, function (err, data) {
+      if (err) {
+        console.log(err);
+        return reject(err);
+      }
+      resolve(data);
+    });
+  });
+}
+function get (url) {
+  return new Promise(function (resolve, reject) {
+    let data = '';
+
+    https.get(url, function (res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        data += chunk;
+      });
+      res.on('end', function () {
+        resolve({
+          statusCode: parseInt(res.statusCode, 10),
+          data: JSON.parse(data)
+        });
+      });
+    }).on('error', reject);
+  });
+}
+
+exports.handler = function (event, context) {
+  return get(ENDPOINT)
+    .then(function (res) {
+      if (res.statusCode === 200) {
+        const payload = JSON.stringify(res.data);
+        const filename = `station-status_${res.data.last_updated}.json`;
+        return writeObject(filename, payload);
+      } else {
+        throw new Error(`Request failed. Status code: ${res.statusCode}.\n${res.data}`);
+      }
+    })
+    .catch(console.log);
+};
